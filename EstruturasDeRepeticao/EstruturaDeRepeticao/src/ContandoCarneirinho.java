41e94286de9a48bd0bd4b24bdd2048dff96c4d5a
Sofia Mendes <sofiamendez.amorim99@outlook.com.br>
2024-09-13 09:02:05 -0300

    estrutura de repitação for

---
+++ EstruturasDeRepeticao/EstruturaDeRepeticao/src/ContandoCarneirinho.java
@@ -0,0 +1,6 @@
+public class ContandoCarneirinho {
+    public static void main(String[] args) throws Exception {
+        for (int caneirinho = 1; caneirinho <=20; caneirinho ++)
+        System.out.println(caneirinho + "carneirinho");
+    }
+}
